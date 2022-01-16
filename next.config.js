129da140a5624392db467d1f737dfe72b7511da0
Gulrez <sidd5312@gmailcom>
2022-01-16 20:06:47 +0530

    add npm i

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
     images: {
         domains: ["links.papareact.com"],
     },
+//       serverRuntimeConfig: {
+//     // Will only be available on the server side
+//     mySecret: 'secret',
+//     secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+//   },
+//   publicRuntimeConfig: {
+//     // Will be available on both server and client
+//     staticFolder: '/static',
+//   },
+
 }
